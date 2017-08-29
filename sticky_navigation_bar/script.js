de482fb8f4fe34f22ec98aa957c4614049f97806
Arjun Ghimire <arjunghimire0714@lumbinitechnology.com>
2017-08-29 13:06:26 +0545

    stricky navigationbar

---
+++ sticky_navigation_bar/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+	// define varible
+	var navOffset, scrollPosition = 0;
+	navOffset = $(".nav").offset().top;
+
+	$(window).scroll(function(){
+	    scrollPosition = $(window).scrollTop();
+
+	    if(scrollPosition >= navOffset){
+	    	$(".nav").addClass("fixed");
+	    }
+	    else{
+	    	$(".nav").removeClass("fixed");
+	    }
+
+	});
+});
